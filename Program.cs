d3efd5a76769dba05d6f53f06669e2ccc65d7aeb
Karol Kącki <karol.kacki2@gmail.com>
2024-03-14 21:35:57 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using DesignPatterns.Singleton;
+using System;
+
+namespace DesignPatterns
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            SingletonDemo();
+
+        }
+
+        static void SingletonDemo()
+        {
+            Database db = Database.GetInstance();
+            Database db2 = Database.GetInstance();
+        }
+    }
+}
---
+++ Singleton/Database.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Singleton
+{
+    public class Database
+    {
+        private static Database instance;
+
+        private Database() 
+        {
+            Debug.WriteLine("Conecting to the database server");
+        }
+
+        public static Database GetInstance()
+        {
+            if(instance == null)
+                instance = new Database();
+            return instance;
+        }
+    }
+}
